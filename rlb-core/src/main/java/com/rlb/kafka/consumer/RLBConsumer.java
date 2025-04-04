f95738fa2e3d8cae29ab0bf6083ab9772c08ea14
Avinash <khadsanavinash01@gmail.com>
2025-04-04 22:21:50 +0530

    RLB-master added consumer

--- rlb-core/src/main/java/com/rlb/kafka/consumer/RLBConsumer.java
+++ rlb-core/src/main/java/com/rlb/kafka/consumer/RLBConsumer.java
@@ -1,4 +1,17 @@
 package com.rlb.kafka.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RLBConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RLBConsumer.class);
+
+    @KafkaListener(topics = "rlb_output", groupId = "rlbGroup")
+    public void outputConsumer(String msg) {
+        LOGGER.info(String.format("Messages Received -> %s", msg));
+    }
 }

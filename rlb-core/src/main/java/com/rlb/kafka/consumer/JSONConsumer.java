03eb1a262b91150cb24d3e82a5a7d7e3c4fd90a9
Avinash <khadsanavinash01@gmail.com>
2025-04-11 22:12:21 +0530

    RLB-master added JSON COnsumer

---
+++ rlb-core/src/main/java/com/rlb/kafka/consumer/JSONConsumer.java
@@ -0,0 +1,17 @@
+package com.rlb.kafka.consumer;
+
+import com.rlb.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JSONConsumer {
+    private static Logger LOGGER = LoggerFactory.getLogger(JSONConsumer.class);
+
+    @KafkaListener(topics = "json_topic", groupId = "rlbGroup")
+    public void JsonPayloadConsume(User payload) {
+        LOGGER.info(String.format("JSON payload received ->%s", payload));
+    }
+}
